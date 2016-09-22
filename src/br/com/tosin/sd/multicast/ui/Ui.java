732d74c77eada20ebb844193fc92f9205175fbe4
Roger Tosin <roger.tosin@gmail.com>
2016-09-22 01:29:38 -0300

    problema de varias threads resolvido

--- src/br/com/tosin/sd/multicast/ui/Ui.java
+++ src/br/com/tosin/sd/multicast/ui/Ui.java
@@ -3,19 +3,24 @@ package br.com.tosin.sd.multicast.ui;
 import java.util.Scanner;
 
 public class Ui {
-	
+
+	public void showSimpleMessage(String msg) {
+		System.out.println(msg);
+	}
+
 	/**
 	 * Pega a palavra digitada pelo usuario
+	 * 
 	 * @return Palavra
 	 */
 	public String getUiWord() {
 		String result = "";
-		
+
 		System.out.println("Senao souber a palavra digite 0");
 		System.out.print("Qual eh a palavra: \n");
 		@SuppressWarnings("resource")
 		Scanner ler = new Scanner(System.in);
-		
+
 		result = ler.nextLine();
 		System.out.println("\n");
 
@@ -24,110 +29,114 @@ public class Ui {
 
 	/**
 	 * Pega a letra digitada pelo usuario
+	 * 
 	 * @return Letra
 	 */
 	public String getUiLetter() {
 		String result = "";
-		
-		do{
-			//TODO por tosin [12 de set de 2016] tem que verficicar se eh uma letra, nao um simbolo
-		
+
+		do {
+			// TODO por tosin [12 de set de 2016] tem que verficicar se eh uma
+			// letra, nao um simbolo
+
 			System.out.print("Digite uma letra: \n");
 			@SuppressWarnings("resource")
 			Scanner ler = new Scanner(System.in);
-			
+
 			result = ler.nextLine();
 			System.out.println("\n");
-			
-			if(result.length() != 1) {
+
+			if (result.length() != 1) {
 				System.out.println("Nao foi digita uma unica letra!!");
 			}
-		}while(result.length() != 1);
+		} while (result.length() != 1);
 
 		return result;
 	}
-	
+
 	/**
 	 * Mostra mensagem de letra ja escolhida
 	 */
 	public void setUiLetterAlreadChoisen() {
 		System.out.println("Essa letra ja foi escolhida\n");
 	}
-	
+
 	/**
 	 * Mostra mensagem de letra errada
 	 */
 	public void setUiLetterWrong() {
 		System.out.println("Essa palavra nao contem essa letra\n");
 	}
-	
+
 	/**
 	 * Mostra mensagem de letra acertada
 	 */
 	public void setUiLetterRight() {
 		System.out.println("Parabéns voce acertou essa letra\n");
 	}
-	
+
 	/**
 	 * Mostra a pontuacao de todos os jogadores
+	 * 
 	 * @param punctuation
 	 */
 	public void showPunctuation(String punctuation) {
 		String result = "";
-		
+
 		String[] temp = punctuation.split(";");
-		
+
 		result += "\n";
 		result += "=================================================\n";
 		result += "Palavra: " + temp[0] + "\n";
 		result += "=================================================\n";
 		result += "Letras ja utilizadas:\n";
-		
+
 		int i = 1;
-		
+
 		while (i < temp.length) {
-			if(temp[i].equals("PUNCTUATION")) {
+			if (temp[i].equals("PUNCTUATION")) {
 				i++;
 				break;
 			}
 			result += temp[i] + " ";
 			i++;
 		}
-		
+
 		result += "\n=================================================\n";
-		
+
 		result += "\n";
 		result += "JOGADOR\t\t" + "PONTOS";
 		result += "\n";
-		
-		while(i < temp.length) {
-			result += temp[i] + "\t" + temp[i+1] + "\n";
+
+		while (i < temp.length) {
+			result += temp[i] + "\t" + temp[i + 1] + "\n";
 			i += 2;
 		}
-		
+
 		result += "\n\n";
 
 		System.out.println(result);
 	}
-	
+
 	public void GameOver() {
 		System.out.println("\n=======================================================");
 		System.out.println("GAME OVER!");
 		System.out.println("=======================================================\n");
 	}
-	
+
 	public void finish(String id) {
 		System.out.println("\n=======================================================");
 		System.out.println("Acabou!");
 		System.out.println("O jogador " + id + " ganhou!!");
 		System.out.println("=======================================================\n");
 	}
-	
+
 	/**
 	 * Moostra o tempo expirou
+	 * 
 	 * @param id
 	 */
 	public void timeEnd(String id) {
-		
+
 	}
 }
--- src/br/com/tosin/sd/multicast/utils/Constants.java
+++ src/br/com/tosin/sd/multicast/utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
 	public static final String ADDRESS = "228.5.6.7";
 	public static final int PORT = 6789;
-	
+
 	public static final String INITIAL_HANDSHAKE = "Has some one on grup?";
 	public static final String I_AM_A_MASTER = "I am a master";
 	public static final String I_AM_NOT_A_MASTER = "I am not a master";
@@ -34,5 +34,5 @@ public class Constants {
 	public static final String LETTER_ALREAD_CHOSEN = "Letter alread chosen";
 	public static final String LETTER_WRONG = "Letter wrong";
 	public static final String LETTER_RIGHT = "Letter right";
-	
+
 }
